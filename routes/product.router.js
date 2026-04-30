ff9921649dcf69cdfabc49c49acfa90d4369656b
Kang MarguZ <kangmarguz@gmail.com>
2026-04-30 16:31:23 +0700

    remove comment.

--- routes/product.router.js
+++ routes/product.router.js
@@ -7,12 +7,10 @@ const route = express.Router();
 export const isAdmin = authorizeRoles('aDmIN');
 export const isUser = authorizeRoles('uSeR');
 
-// 1 check user auth
-// 2 check user role
-// if pass all just created product.
 route.post('/create-product', authCheck, isUser, createProduct);
 route.post('/booking', authCheck, createVanBooking);
 route.get('/history/:id', getHistory);
-route.put('/history/:id', getHistory);
+route.put('/history/:id', getHistory);  
 route.delete('/history/:id', deleteHistory);
+
 export default route;
